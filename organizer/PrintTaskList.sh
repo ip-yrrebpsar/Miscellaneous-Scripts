ceef4b137c6f0247a5ffa11bda945f0285e3f9e2
pc <>
2022-05-08 12:21:15 -0400

    Organizer: added PrintTaskList first draft

---
+++ organizer/PrintTaskList.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#set -x
+#set -e
+today=`date '+%m/%d/%Y'`
+printHeader(){
+    #Jan 1 2050 -> 01/01/2050
+    dateMMDDYYYY=`date -d  "$dateFormat" "+%m/%d/%Y"`
+    header=`egrep "$dateMMDDYYYY" ./dateMasks | cut -d ":" -f 3`
+    if [ -z "$header" ] ; then 
+       if [ "$dateMMDDYYYY" == "$today" ] ; then 
+          echo Due Today
+       else
+          echo "$dateFormat"
+       fi
+    else
+       echo $header
+    fi
+}
+
+while IFS= read -r line; do
+	id=`echo $line |  awk '{ print $1; } '`
+	processCommand=`at -c $id | tail -n2 | head -n1 | cut -f 2- -d ';' | tr -d \" ` #eg ./ProcessTask.sh 23:59 05/08/2022 2 task4
+ prio=`echo $processCommand | awk '{print $4}'`
+ task=`echo $processCommand | cut -d ' ' -f 5-`
+ taskFormat="p$prio id:$id | $task "
+
+ dateFormat=`echo $line | awk '{print $3 " " $4 " " $6 }' ` # #May 8 2022
+
+			if [ "$dateFormat" != "$lastDate" ] ; then
+				echo "" 
+    printHeader
+    lastDate="$dateFormat"
+			fi
+			echo  "    $taskFormat"
+
+              #sorts atq by time
+done < <(atq | sort -k 6n -k 3M -k 4n -k 5 -k 7 -k 1 )
